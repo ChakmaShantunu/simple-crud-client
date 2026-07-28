906cf9783a7cf3c8f25b2d0b820a244bace8bf3c
shantunu007 <chakmashantunu.web@gmail.com>
2026-07-28 10:19:56 +0600

    Set up React Router with main layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router";
+import MainLayout from './layouts/Mainlayout.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: MainLayout,
+    children: [
+      {
+        index: true,
+        Component: App
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )
